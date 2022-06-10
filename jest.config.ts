c1b9d0b758bb2cb5dc6e6430d834e92271fceb7f
Lucas Esteves dos Reis <Lucas.Salgado@thomsonreuters.com>
2022-06-09 23:43:31 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
