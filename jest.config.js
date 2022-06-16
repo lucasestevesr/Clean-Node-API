71462bf1299e4098334449b7597d891508ce022c
Lucas Esteves dos Reis <Lucas.Salgado@thomsonreuters.com>
2022-06-16 11:36:48 -0300

    chore: jest cofings

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src'],
   coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
