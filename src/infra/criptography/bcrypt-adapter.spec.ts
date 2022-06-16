1f7b7c94eac0c9b7c16711c9f6a61351d4a81ea9
Lucas Esteves dos Reis <Lucas.Salgado@thomsonreuters.com>
2022-06-16 19:03:59 -0300

    test: ensure BcryptAdapter throws if bcrypt throws

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -7,10 +7,10 @@ jest.mock('bcrypt', () => ({
   }
 }))
 
-const salt = 12
+const SALT = 12
 
 const makeSut = (): BcryptAdapter => {
-  return new BcryptAdapter(salt)
+  return new BcryptAdapter(SALT)
 }
 
 describe('Bcrypt Adapter', () => {
@@ -18,12 +18,21 @@ describe('Bcrypt Adapter', () => {
     const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
-    expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+    expect(hashSpy).toHaveBeenCalledWith('any_value', SALT)
   })
 
   test('Should return a hash on success', async () => {
     const sut = makeSut()
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('hash')
+  })
+
+  test('Should throw if bcrypt throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
   })
 })
